f9f9395fbd91b813f0481dfff30e273e23484902
Leela07 <krishnamurthy63043@gmail.com>
2025-07-02 22:03:49 +0530

    02-07-2025

---
+++ Javascript/DOM_Practice-3/script.js
@@ -0,0 +1,45 @@
+// function createPara ()
+// {
+//     const para = document.createElement('p');
+//     para.textContent = "PRASAD"
+//     document.body.appendChild(para)
+// }
+// function createPara2 ()
+// {
+//     for (let i = 0; i <= 10; i++)
+//     {
+//     const para = document.createElement('p'); //<p></p>
+//     para.textContent = "PRASAD" //
+//     document.body.appendChild(para)
+//     }
+// }
+// createPara2();
+
+
+
+
+
+
+
+const movies = [
+    {
+       Title: "RRR"
+    },
+    {
+       Title:"Pushpa"
+    },
+    {
+       Title:"Salaar"
+    }
+]
+const mainEle = document.getElementById('main');
+function createMovies ()
+{
+    movies.forEach(function (ele,ind)
+    {
+        const para = document.createElement('p');//<p></p>
+        para.textContent = ele.Title;
+        mainEle.appendChild(para)
+    })
+}
+createMovies()
